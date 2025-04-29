721bc7c9b39380c870fd41c63129155d66b0157d
reese-mil <113038226+reese-mil@users.noreply.github.com>
2025-04-28 22:05:07 -0400

    Update wma_calculator.v

--- WMA_Calculator/wma_calculator.v
+++ WMA_Calculator/wma_calculator.v
@@ -4,19 +4,19 @@
 
 module WMA_Calculator (
 	input wire [7:0] x,        	// current temp input
-    input wire [7:0] WMA0,     	// previous WMA value
+    input wire [7:0] WMA0,     		// previous WMA value
 	input wire [7:0] T1,       	// low threshold
 	input wire [7:0] T2,       	// high threshold
 	output reg [7:0] WMA1      	// new WMA output
 );
 
-    // internal registers for weights
+    	// internal registers for weights
 	reg [1:0] w1, w0;
 	
 	// internal registers for numerator and denominator
 	reg [7:0] num, denom;
 
-	always @(*) begin           // updates any time there's an input change
+	always @(*) begin           	// updates when input change
     	// determine weight w1 based on temperature range
     	if (x < T1) 
         	w1 = 1;
@@ -25,7 +25,7 @@ module WMA_Calculator (
     	else 
         	w1 = 1;		        // default case
 
-        w0 = w1;                // always the same since no variables differ in calculation
+        w0 = w1;                	// always the same since no variables differ in calculation
 
 
     	//numerator without multiplication
@@ -39,9 +39,9 @@ module WMA_Calculator (
 		denom = w1 + w0;
 
 		case (denom)
-			2:	WMA1 = num >> 1;			        //divide by 2
-			6:	WMA1 = (num >> 3) + (num >> 4);	    //divide by ~6
-			default: WMA1 = num >> 1;			    //default case divides by 2		
+			2:	WMA1 = num >> 1;			//divide by 2
+			6:	WMA1 = (num >> 3) + (num >> 4);	    	//divide by ~6
+			default: WMA1 = num >> 1;			//default case divides by 2		
 		endcase
 	end
 endmodule
